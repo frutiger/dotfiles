1b3b217b570acb1034d643ff253746f7c620b212
Masud Rahman <mail@masudrahman.com>
2023-04-11 12:12:25 -0400

    win: ignore ctrl+z in NeoVim
    Ctrl+Z suspends the process in Windows Terminal and does not provide a
    way to resume it.

--- win/up/.config/nvim/init.lua
+++ win/up/.config/nvim/init.lua
@@ -2,3 +2,7 @@
 vim.opt.wildignore = { "*.exe", "*.dll", "*.obj", "*.lib" }
 vim.opt.shell      = "C:\\\\Windows\\\\System32\\\\cmd.exe"
 
+-- ignore ctrl+z since it suspends the process and Windows Terminal can't
+-- resume it
+vim.keymap.set({'n', 'v', 'o', 'i'}, '<C-z>', function () end)
+
